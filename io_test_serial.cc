ebb64b09c9f834d562699a08d8bb9b2b77459fd8
Casey W. Stark <caseywstark@gmail.com>
2014-12-12 12:24:52 -0800

    Serial version updates. Simpler buffering, minor changes to hdf5 version

--- io_test_serial.cc
+++ io_test_serial.cc
@@ -25,36 +25,31 @@ class Timer {
 };
 
 /*
-Writes the `values` array of size `n` to the file path `output_path`. The IO
-is done in chunks of size `n_buffer`.
+Writes a 3D grid scalar `values` with shape `(n, n, n)` to `output_path`.
+IO is done in chunks of `n * n`.
 */
-void write_double_array_to_float(const int n, const double * const values,
-    const int n_buffer, float * const buffer,
+void write_custom(const int n, const double * const values,
     const string output_path)
 {
-    const int nn = n * n * n;
+    const int block_size = n * n;
+    const int num_blocks = n;
+    float *io_buffer = new float[block_size];
+
     // use C-style IO.
     FILE *output_file = fopen(output_path.c_str(), "w");
     // check that open was successful.
     if (output_file) {
-        // start buffered write.
-        int n_written = 0;
-        const double *values_pos = values;
-        while (n_written < nn) {
-            // write chunks with n_buffer size until we reach the end.
-            int n_write = min(n_buffer, nn - n_written);
-            // pointers to current buffer index and the end.
-            float *buffer_pos = buffer;
-            float *buffer_end = buffer + n_write;
-            // copy values to buffer, advancing ptrs.
-            while (buffer_pos != buffer_end) { *buffer_pos++ = *values_pos++; }
-
-            fwrite(buffer, sizeof(float), n_write, output_file);
-
-            // update count.
-            n_written += n_write;
+        // i0 tracks the current offset in values.
+        int i0 = 0;
+        for (int iblock = 0; iblock < num_blocks; ++iblock) {
+            // Copy to I/O buffer.
+            for (int i = 0; i < block_size; ++i) {
+                io_buffer[i] = values[i0 + i];
+            }
+            i0 += block_size;
+            fwrite(io_buffer, sizeof(float), block_size, output_file);
         }
-
+        // Close file.
         fclose(output_file);
     }
     else {
@@ -68,7 +63,7 @@ Creates HDF5 file `output_path` and writes `values` to a single dataset,
 with a rank of `num_dims` and shape `ds_shape`
 (that is, `ds_shape` should have `num_dims` elements).
 */
-void write_double_array_hdf5(const int num_dims, const hsize_t * const ds_shape,
+void write_hdf5(const int num_dims, const hsize_t * const ds_shape,
     const double * const values, const string output_path)
 {
     const string dataset_path = "data";
@@ -120,17 +115,18 @@ main(int argc, char **argv)
     }
 
     // Method A
-    const int n_buffer = 1024*1024;
-    float *buffer = new float[n_buffer];
+    cout << "Running method A." << endl;
 
     Timer ta;
     ta.start();
     for (int s = 0; s < num_samples; ++s) {
-        write_double_array_to_float(n, data, n_buffer, buffer, "test.bin");
+        write_custom(n, data, "test.bin");
     }
     double total_time_a = ta.elapsed();
 
     // Method B
+    cout << "Running method B." << endl;
+
     int num_dims = 3;
     hsize_t shape[3];
     shape[0] = n; shape[1] = n; shape[2] = n;
@@ -138,7 +134,7 @@ main(int argc, char **argv)
     Timer tb;
     tb.start();
     for (int s = 0; s < num_samples; ++s) {
-        write_double_array_hdf5(num_dims, shape, data, "test.h5");
+        write_hdf5(num_dims, shape, data, "test.h5");
     }
     double total_time_b = tb.elapsed();
 
